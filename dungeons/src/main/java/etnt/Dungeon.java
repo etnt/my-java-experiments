ff0284c23151bd0012b20514202c03b7076ce592
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-01-04 20:30:44 +0000

    Adding Player and handling of Items

--- dungeons/src/main/java/etnt/Dungeon.java
+++ dungeons/src/main/java/etnt/Dungeon.java
@@ -1,12 +1,19 @@
 package etnt;
 
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Dungeon {
     public static void main(String[] args) {
         Room room1 = new Room("Room 1", "You are in a dark room. There is a door to the east.");
+        room1.addItem(new Item("key"));
+        room1.addItem(new Item("sword"));
+
         Room room2 = new Room("Room 2", "You are in a dark room. There is a door to the west and a door to the east.");
+        room2.addItem(new Item("potion"));
+
         Room room3 = new Room("Room 3", "You are in a dark room. There is a door to the west.");
+        room3.addItem(new Item("coins"));
 
         Door door1 = new Door(room1, room2);
         Door door2 = new Door(room2, room3);
@@ -16,25 +23,55 @@ public class Dungeon {
         room2.addDoor("east", door2);
         room3.addDoor("west", door2);
 
+        Player player = new Player(100);  // player starts with 100 health points
+
         Room currentRoom = room1;
         System.out.println("You are in " + currentRoom.getName());
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter a direction (east or west) or 'quit' to exit:");
-            String direction = scanner.nextLine();
+            System.out.println("Enter a command (go, look, take, drop, inventory, health, quit):");
+            String command = scanner.nextLine();
 
-            if (direction.equalsIgnoreCase("quit")) {
+            if (command.equalsIgnoreCase("quit")) {
                 break;
-            }
-
-            Room nextRoom = currentRoom.go(direction);
-            if (nextRoom != null) {
-                currentRoom = nextRoom;
-                System.out.println("You go " + direction + ". You are now in " + currentRoom.getName());
+            } else if (command.startsWith("go ")) {
+                String direction = command.substring(3);
+                Room nextRoom = currentRoom.go(direction);
+                if (nextRoom != null) {
+                    currentRoom = nextRoom;
+                    System.out.println("You go " + direction + ". You are now in " + currentRoom.getName());
+                    System.out.println(currentRoom.getDescription());
+                    System.out.println("You see the following items: " + currentRoom.getItems().stream().map(Item::getName).collect(Collectors.joining(", ")));
+                } else {
+                    System.out.println("There is no door in that direction.");
+                }
+            } else if (command.startsWith("take ")) {
+                String itemName = command.substring(5);
+                Item item = currentRoom.removeItem(itemName);
+                if (item != null) {
+                    player.addItem(item);
+                    System.out.println("You take the " + itemName + ".");
+                } else {
+                    System.out.println("There is no " + itemName + " here.");
+                }
+            } else if (command.startsWith("drop ")) {
+                String itemName = command.substring(5);
+                if (player.removeItem(itemName)) {
+                    currentRoom.addItem(new Item(itemName));
+                    System.out.println("You drop the " + itemName + ".");
+                } else {
+                    System.out.println("You don't have a " + itemName + ".");
+                }
+            } else if (command.equalsIgnoreCase("inventory")) {
+                System.out.println("You are carrying: " + player.getInventory().stream().map(Item::getName).collect(Collectors.joining(", ")));
+            } else if (command.equalsIgnoreCase("health")) {
+                System.out.println("You have " + player.getHealthPoints() + " health points.");
+            } else if (command.equalsIgnoreCase("look")) {
                 System.out.println(currentRoom.getDescription());
+                System.out.println("You see the following items: " + currentRoom.getItems().stream().map(Item::getName).collect(Collectors.joining(", ")));
             } else {
-                System.out.println("There is no door in that direction.");
+                System.out.println("I don't understand that command.");
             }
         }
         scanner.close();
--- dungeons/src/main/java/etnt/Player.java
+++ dungeons/src/main/java/etnt/Player.java
@@ -2,34 +2,40 @@ package etnt;
 
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 public class Player {
-    private Room currentRoom;
     private ArrayList<Item> inventory;
+    private int healthPoints;
 
-    public Player(Room startingRoom) {
-        this.currentRoom = startingRoom;
+    public Player(int healthPoints) {
         this.inventory = new ArrayList<>();
+        this.healthPoints = healthPoints;
     }
 
-    public void move(String direction) {
-        Room nextRoom = currentRoom.go(direction);
-        if (nextRoom != null) {
-            currentRoom = nextRoom;
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+
+    public boolean removeItem(String itemName) {
+        for (Item item : inventory) {
+            if (item.getName().equals(itemName)) {
+                inventory.remove(item);
+                return true;
+            }
         }
-    }
-
-    public void pickUpItem(String itemName) {
-        Item item = currentRoom.removeItem(itemName);
-        if (item != null) {
-            inventory.add(item);
-        }
-    }
-
-    public Room getCurrentRoom() {
-        return currentRoom;
+        return false;
     }
 
     public ArrayList<Item> getInventory() {
         return inventory;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
     }
 }
